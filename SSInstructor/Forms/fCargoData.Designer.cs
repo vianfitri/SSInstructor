94efe9f5daa5fb8201e5b11fd51077bb89c94291
vianfitri <viancukiep@gmail.com>
2022-06-09 11:56:43 +0700

    commit

--- SSInstructor/Forms/fCargoData.Designer.cs
+++ SSInstructor/Forms/fCargoData.Designer.cs
@@ -132,6 +132,7 @@ namespace SSInstructor.Forms
             // 
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.bdgv_cargo.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.bdgv_cargo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
