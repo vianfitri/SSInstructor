ef4fc6655af92e79865c63992c306473df4579e7
vianfitri <viancukiep@gmail.com>
2022-06-09 13:31:04 +0700

    commit

---
+++ SSInstructor/Forms/fAddInstructor.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SSInstructor.Forms
+{
+    partial class fAddInstructor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.viTextBox1 = new ViControls.ViTextBox();
+            this.viTextBox2 = new ViControls.ViTextBox();
+            this.viTextBox3 = new ViControls.ViTextBox();
+            this.btnAdd = new ViControls.ViButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NIP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Level";
+            // 
+            // viTextBox1
+            // 
+            this.viTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.viTextBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.viTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.viTextBox1.BorderRadius = 10;
+            this.viTextBox1.BorderSize = 2;
+            this.viTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.viTextBox1.Location = new System.Drawing.Point(161, 50);
+            this.viTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.viTextBox1.Multiline = false;
+            this.viTextBox1.Name = "viTextBox1";
+            this.viTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.viTextBox1.PasswordChar = false;
+            this.viTextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.viTextBox1.PlaceholderText = "";
+            this.viTextBox1.Size = new System.Drawing.Size(250, 35);
+            this.viTextBox1.TabIndex = 3;
+            this.viTextBox1.Texts = "";
+            this.viTextBox1.UnderlinedStyle = false;
+            // 
+            // viTextBox2
+            // 
+            this.viTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.viTextBox2.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.viTextBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.viTextBox2.BorderRadius = 10;
+            this.viTextBox2.BorderSize = 2;
+            this.viTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viTextBox2.ForeColor = System.Drawing.Color.DimGray;
+            this.viTextBox2.Location = new System.Drawing.Point(161, 99);
+            this.viTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.viTextBox2.Multiline = false;
+            this.viTextBox2.Name = "viTextBox2";
+            this.viTextBox2.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.viTextBox2.PasswordChar = false;
+            this.viTextBox2.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.viTextBox2.PlaceholderText = "";
+            this.viTextBox2.Size = new System.Drawing.Size(250, 35);
+            this.viTextBox2.TabIndex = 4;
+            this.viTextBox2.Texts = "";
+            this.viTextBox2.UnderlinedStyle = false;
+            // 
+            // viTextBox3
+            // 
+            this.viTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.viTextBox3.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.viTextBox3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.viTextBox3.BorderRadius = 10;
+            this.viTextBox3.BorderSize = 2;
+            this.viTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viTextBox3.ForeColor = System.Drawing.Color.DimGray;
+            this.viTextBox3.Location = new System.Drawing.Point(161, 147);
+            this.viTextBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.viTextBox3.Multiline = false;
+            this.viTextBox3.Name = "viTextBox3";
+            this.viTextBox3.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.viTextBox3.PasswordChar = false;
+            this.viTextBox3.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.viTextBox3.PlaceholderText = "";
+            this.viTextBox3.Size = new System.Drawing.Size(250, 35);
+            this.viTextBox3.TabIndex = 5;
+            this.viTextBox3.Texts = "";
+            this.viTextBox3.UnderlinedStyle = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnAdd.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnAdd.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnAdd.BorderRadius = 10;
+            this.btnAdd.BorderSize = 0;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(196, 218);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 40);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "btnAdd";
+            this.btnAdd.TextColor = System.Drawing.Color.White;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // fAddInstructor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 307);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.viTextBox3);
+            this.Controls.Add(this.viTextBox2);
+            this.Controls.Add(this.viTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fAddInstructor";
+            this.Text = "fAddInstructor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private ViControls.ViTextBox viTextBox1;
+        private ViControls.ViTextBox viTextBox2;
+        private ViControls.ViTextBox viTextBox3;
+        private ViControls.ViButton btnAdd;
+    }
+}
