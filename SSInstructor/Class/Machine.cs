2c73a8a6dde74f87d100175cdd6a6c55ec249e79
vianfitri <viancukiep@gmail.com>
2022-06-20 15:28:05 +0700

    Add Machine Class

---
+++ SSInstructor/Class/Machine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SSInstructor.Class
+{
+    [DebuggerDisplay("{Name,nq}")]
+    [Serializable]
+    public class Machine
+    {
+        public enum ShutdownMethods
+        {
+            WMI,
+            Custom,
+            Legacy
+        }
+
+        public enum StatusCodes {
+            Online,
+            Offline,
+            Unknown,
+            Fail,
+            Uninitialized
+        }
+
+        public enum TraceMethods {
+            WriteLine,
+            Indent,
+            UnIndent
+        }
+    }
+}
