a80e84f8146403b13e4240cffee5a1a8bc35fd39
vianfitri <viancukiep@gmail.com>
2022-07-08 10:02:17 +0700

    commit

---
+++ SSInstructor/Class/moduleMySQL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SSInstructor.Class
+{
+    class moduleMySQL
+    {
+        private MySqlDataAdapter mySQLAdapter;
+        private MySqlConnection mySQLConn;
+        private MySqlCommand mySQLCmd;
+        private MySqlDataReader mySQLReader;
+        private MySqlTransaction mySQLTransact;
+
+        private string conn = "Server=localhost; port=3306; Uid=root; Pwd=admin123!; database=";
+        public string errMessage;
+    }
+}
