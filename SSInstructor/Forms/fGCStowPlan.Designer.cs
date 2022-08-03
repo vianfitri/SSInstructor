dbdfc4dd427f8589097c803f3e7cc4f46ffedefb
vianfitri <viancukiep@gmail.com>
2022-08-03 10:33:37 +0700

    commit

--- SSInstructor/Forms/fGCStowPlan.Designer.cs
+++ SSInstructor/Forms/fGCStowPlan.Designer.cs
@@ -30,6 +30,7 @@ namespace SSInstructor.Forms
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,23 @@ namespace SSInstructor.Forms
             this.label1.TabIndex = 0;
             this.label1.Text = "Stowage Plan";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(118)))), ((int)(((byte)(159)))));
+            this.label2.Location = new System.Drawing.Point(60, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 23);
+            this.label2.TabIndex = 211;
+            this.label2.Text = "3650 DWT General Cargo";
+            // 
             // fGCStowPlan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(896, 721);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "fGCStowPlan";
             this.Text = "fGCStowPlan";
@@ -58,5 +71,6 @@ namespace SSInstructor.Forms
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
