77b1740b95672a88473b5f852110c6653acc3c32
vianfitri <viancukiep@gmail.com>
2022-09-23 10:38:11 +0700

    commit

---
+++ Machines/Machine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machines
+{
+    public class Machine
+    {
+        #region "Fields"
+        #endregion
+
+        #region "Constructor"
+        public Machine()
+        {
+
+        }
+        #endregion
+
+        #region "Properties"
+        #endregion
+
+        #region "Methode"
+        #endregion
+
+        #region "Events"
+        #endregion
+    }
+}
