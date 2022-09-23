1caf28340a6a4a6a7dd8806db3e8d595e1b77e05
vianfitri <viancukiep@gmail.com>
2022-09-23 13:03:13 +0700

    commit

--- Machines/Machine.cs
+++ Machines/Machine.cs
@@ -8,30 +8,64 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Machines
 {
-    [Serializable]
+    [Serializable()]
     public class Machine
     {
         #region "Fields"
+        private BackgroundWorker _backgroundWorker = new BackgroundWorker();
+        private Ping ping = new Ping();
+        
+        public enum StatusCodes
+        {
+            Online,
+            Offline,
+            Unknown,
+            Fail,
+            Uninitialized
+        }
+
+        private string name = string.Empty;
+        private string mac = string.Empty;
+        private string ip = string.Empty;
+
+        [NonSerialized()] public StatusCodes Status = StatusCodes.Uninitialized;
+        [NonSerialized()] public PingReply Reply;
+        [NonSerialized()] public Semaphore Pool;
+
         #endregion
 
         #region "Constructor"
         public Machine()
         {
-
+            _backgroundWorker.WorkerSupportsCancellation = true;
         }
         #endregion
 
         #region "Properties"
+        public string Name { get => name; set => name = value; }
+        public string Mac { get => mac; set => mac = value; }
+        public string IP { get => ip; set => ip = value; }
         #endregion
 
         #region "Methode"
+        public void Run()
+        {
+            if (_backgroundWorker.IsBusy)
+                return;
+            _backgroundWorker.WorkerReportsProgress = true;
+            if (string.IsNullOrEmpty(IP))
+                return;
+            _backgroundWorker.RunWorkerAsync(IP);
+        }
         #endregion
 
         #region "Events"
+
         #endregion
     }
 }
