5653169a6a67e516462ceff31093009b0d7b4698
vianfitri <viancukiep@gmail.com>
2022-09-23 10:59:08 +0700

    commit

--- Machines/Machine.cs
+++ Machines/Machine.cs
@@ -4,13 +4,15 @@ using System.ComponentModel;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Machines
 {
+    [Serializable]
     public class Machine
     {
         #region "Fields"
