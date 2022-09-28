238d6bb1885cef9389956dc288f22f2702100950
vianfitri <viancukiep@gmail.com>
2022-09-28 09:54:15 +0700

    commit

--- SSInstructor/Forms/fShipData.cs
+++ SSInstructor/Forms/fShipData.cs
@@ -36,64 +36,75 @@ namespace SSInstructor.Forms
         {
             InitializeComponent();
 
-			//try
-			//{
-			//	process = new Process();
-			//	process.StartInfo.FileName = "bc.exe";
-			//	process.StartInfo.Arguments = "-parentHWND " + pnlShipView.Handle.ToInt32() + " " + Environment.CommandLine;
-			//	process.StartInfo.UseShellExecute = true;
-			//	process.StartInfo.CreateNoWindow = true;
+			try
+			{
+				process = new Process();
+				process.StartInfo.FileName = Application.StartupPath + "\\3D\\3DViewBulkcarrier.exe";
+				process.StartInfo.Arguments = "-parentHWND " + pnlShipView.Handle.ToInt32() + " " + Environment.CommandLine;
+				process.StartInfo.UseShellExecute = true;
+				process.StartInfo.CreateNoWindow = true;
 
-			//	process.Start();
+				process.Start();
 
-			//	process.WaitForInputIdle();
-			//	// Doesn't work for some reason ?!
-			//	//unityHWND = process.MainWindowHandle;
-			//	EnumChildWindows(pnlShipView.Handle, WindowEnum, IntPtr.Zero);
-			//}
-			//catch (Exception ex)
-			//{
-			//	MessageBox.Show(ex.Message + ".\nCheck if Container.exe is placed next to Child.exe.");
-			//}
+				process.WaitForInputIdle();
+				// Doesn't work for some reason ?!
+				//unityHWND = process.MainWindowHandle;
+				EnumChildWindows(pnlShipView.Handle, WindowEnum, IntPtr.Zero);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + ".\nCheck if Container.exe is placed next to Child.exe.");
+			}
 		}
 
-	//	private void ActivateUnityWindow()
-	//	{
-	//		SendMessage(modelshipHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
-	//	}
+		private void ActivateUnityWindow()
+		{
+			SendMessage(modelshipHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+		}
 
-	//	private void DeactivateUnityWindow()
-	//	{
-	//		SendMessage(modelshipHWND, WM_ACTIVATE, WA_INACTIVE, IntPtr.Zero);
-	//	}
+		private void DeactivateUnityWindow()
+		{
+			SendMessage(modelshipHWND, WM_ACTIVATE, WA_INACTIVE, IntPtr.Zero);
+		}
 
-	//	private int WindowEnum(IntPtr hwnd, IntPtr lparam)
-	//	{
-	//		modelshipHWND = hwnd;
-	//		ActivateUnityWindow();
-	//		return 0;
-	//	}
+		private int WindowEnum(IntPtr hwnd, IntPtr lparam)
+		{
+			modelshipHWND = hwnd;
+			ActivateUnityWindow();
+			return 0;
+		}
 
-	//	private void fShipData_Resize(object sender, EventArgs e)
-	//	{
-	//		MoveWindow(modelshipHWND, 0, 0, pnlShipView.Width, pnlShipView.Height, true);
-	//		ActivateUnityWindow();
-	//	}
+		private void fShipData_Resize(object sender, EventArgs e)
+		{
+			MoveWindow(modelshipHWND, 0, 0, pnlShipView.Width, pnlShipView.Height, true);
+			ActivateUnityWindow();
+		}
 
-	//	// Close Unity application
-	//	private void fShipData_FormClosed(object sender, FormClosedEventArgs e)
-	//	{
+		// Close Unity application
+		private void fShipData_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			try
+			{
+				process.CloseMainWindow();
 
- //       }
+				Thread.Sleep(1000);
+				while (process.HasExited == false)
+					process.Kill();
+			}
+			catch (Exception)
+			{
 
- //       private void fShipData_Activated(object sender, EventArgs e)
-	//	{
-	//		ActivateUnityWindow();
-	//	}
+			}
+		}
 
-	//	private void fShipData_Deactivate(object sender, EventArgs e)
-	//	{
-	//		DeactivateUnityWindow();
-	//	}
+        private void fShipData_Activated(object sender, EventArgs e)
+		{
+			ActivateUnityWindow();
+		}
+
+		private void fShipData_Deactivate(object sender, EventArgs e)
+		{
+			DeactivateUnityWindow();
+		}
 	}
 }
