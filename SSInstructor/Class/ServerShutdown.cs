7409bfff09ce158180f552126f3982b65cdd88d4
vianfitri <viancukiep@gmail.com>
2022-10-05 13:33:29 +0700

    commit

--- SSInstructor/Class/ServerShutdown.cs
+++ SSInstructor/Class/ServerShutdown.cs
@@ -391,7 +391,7 @@ namespace SSInstructor.Class
             }
             else if (listener == null || !listener.IsAlive)
             {
-                string address = "127.0.0.1";
+                string address = "192.168.10.200";
                 string number = "9000";
                 bool error = false;
                 IPAddress ip = null;
