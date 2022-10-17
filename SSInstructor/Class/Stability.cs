78dc1929344bef922962a616570143f25af780f6
vianfitri <viancukiep@gmail.com>
2022-10-17 12:18:31 +0700

    commit

---
+++ SSInstructor/Class/Stability.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSInstructor.Class
+{
+    public static class Stability
+    {
+        #region "Struct"
+        public struct Point2D
+        {
+            public double x, y;
+        }
+
+        // point double value
+        public struct Point3D
+        {
+            /*
+             * x = longitudinal position
+             * y = transversal position
+             * z = vertical position
+             */
+            public double x, y, z;
+        }
+        #endregion
+
+        #region "Fields"
+        #endregion
+
+        #region "Method"
+        #endregion
+    }
+}
