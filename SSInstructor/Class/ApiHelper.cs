55ca7b4970409c7d429533c9153ca534d2739876
vianfitri <viancukiep@gmail.com>
2022-10-19 15:45:17 +0700

    commit

---
+++ SSInstructor/Class/ApiHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSInstructor.Class
+{
+    public static class ApiHelper
+    {
+        #region "Fields"
+        #endregion
+
+        #region "Constructor"
+        #endregion
+
+        #region "Properties"
+        public static HttpClient ApiClient { get; set; }
+        #endregion
+
+        #region "Method"
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        #endregion
+    }
+}
