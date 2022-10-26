213e88d16d3302992b4a44f50ff56357daf65ba2
vianfitri <viancukiep@gmail.com>
2022-10-26 08:59:08 +0700

    commit

---
+++ SSInstructor/Class/VisualServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSInstructor.Class
+{
+    public static class VisualServer
+    {
+    }
+
+    public class ServerVisual
+    {
+
+    }
+}
