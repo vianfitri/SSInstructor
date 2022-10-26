1bad75d19f07f46c943d223cdf9563aec6fc9667
vianfitri <viancukiep@gmail.com>
2022-10-26 07:05:28 +0700

    commit

---
+++ SSInstructor/Class/ExerciseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSInstructor.Class
+{
+    public static class ExerciseController
+    {
+        #region "Fields"
+        #endregion
+
+        #region "Properties"
+        #endregion
+
+        #region "Method"
+        #endregion
+    }
+}
---
+++ SSInstructor/Class/VisualizationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSInstructor.Class
+{
+    public static class VisualizationController
+    {
+        #region "Fields"
+        #endregion
+
+        #region "Properties"
+        #endregion
+
+        #region "Method"
+        #endregion
+    }
+}
