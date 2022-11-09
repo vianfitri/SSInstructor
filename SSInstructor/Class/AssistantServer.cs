ba1eeed3f0a9d7946e8003f7c320b06392ed166e
vianfitri <viancukiep@gmail.com>
2022-11-09 13:27:37 +0700

    commit

---
+++ SSInstructor/Class/AssistantServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Machines;
+
+namespace SSInstructor.Class
+{
+    public static class AssistantServer
+    {
+    }
+}
